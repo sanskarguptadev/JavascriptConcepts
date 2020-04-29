2fe9725d7daf8bc6b5810fff6a80d4e452178e91
sanskarguptadev <sanskar.gupta.dev@gmail.com>
2020-04-29 10:48:54 +0530

    event capturing

--- EventBubling/eventBubling.js
+++ EventBubling/eventBubling.js
@@ -1,11 +1,11 @@
 document.querySelector("#grand").addEventListener('click', () => {
     console.log('grandparent');
-});
+}, true);
 
 document.querySelector("#parent").addEventListener('click', () => {
     console.log('parent');
-});
+}, true);
 
 document.querySelector("#child").addEventListener('click', () => {
     console.log('child');
-});
+}, true);
